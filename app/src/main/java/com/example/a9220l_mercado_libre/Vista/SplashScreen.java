92fd9210f37f47972746f290028b46b44f43cd8c
dacarrascals <dacarrascals@ufpso.edu.co>
2021-09-10 17:25:45 -0500

    IMPLEMENTACION - Splash Screen

--- app/src/main/java/com/example/a9220l_mercado_libre/Vista/SplashScreen.java
+++ app/src/main/java/com/example/a9220l_mercado_libre/Vista/SplashScreen.java
@@ -1,4 +1,26 @@
 package com.example.a9220l_mercado_libre.Vista;
 
-public class SplashScreen {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.a9220l_mercado_libre.MainActivity;
+import com.example.a9220l_mercado_libre.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            Intent intent= new Intent(SplashScreen.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        },3000);
+
+    }
 }
