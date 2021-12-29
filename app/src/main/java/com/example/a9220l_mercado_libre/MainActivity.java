0c0431687188c78190a152af6b5c63990c8f73bb
Diego Carrascal <diegocarrascal@wposs.com>
2021-12-29 08:35:33 -0500

    IMPLEMENTACIÓN - Funcionalidad Bottom Navigation
    * Se agrega funcionalidad al Bottom Navigation con su respectivo fragment

--- app/src/main/java/com/example/a9220l_mercado_libre/MainActivity.java
+++ app/src/main/java/com/example/a9220l_mercado_libre/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.a9220l_mercado_libre;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.a9220l_mercado_libre.Vista.BuscarFragment;
+import com.example.a9220l_mercado_libre.Vista.HomeFragment;
+import com.example.a9220l_mercado_libre.Vista.SitiosFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigationView =  findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_container,new HomeFragment()).commit();
+        navigationView.setSelectedItemId(R.id.nav_home);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment=null;
+
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        fragment= new HomeFragment();
+                        break;
+                    case R.id.nav_buscador:
+                        fragment= new BuscarFragment();
+                        break;
+                    case R.id.nav_sitios:
+                        fragment= new SitiosFragment();
+                        break;
+
+                }
+
+              getSupportFragmentManager().beginTransaction().replace(R.id.body_container,fragment).commit();
+
+                return true;
+            }
+        });
+
+
+
+
     }
 }
